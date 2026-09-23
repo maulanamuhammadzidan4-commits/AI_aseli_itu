235a915bef7d5288ceb6d86bc2c9a9ec7d564f35
Muhammad_Zidan_Maulana <maulanamuhammadzidan4@gmail.com>
2026-09-23 14:55:41 +0700

    feat(main): menambahkan kode untuk menghentikan program via KeyboardInterrupt

--- main.py
+++ main.py
@@ -32,68 +32,72 @@ if not kamera.isOpened():
     exit()
 
 while True:
-    berhasil, frame = kamera.read()
+    try:
+        berhasil, frame = kamera.read()
 
-    if not berhasil:
-        print("Gagal membaca kamera.")
-        break
-    frame = cv2.flip(frame, 1)
-    frame_rgb = cv2.cvtColor(
-        frame,
-        cv2.COLOR_BGR2RGB
-    )
-    mp_image = mp.Image(
-        image_format=mp.ImageFormat.SRGB,
-        data=frame_rgb
-    )
-    result = landmarker.detect(mp_image)
-
-    arah = "TIDAK TERDETEKSI"
-
-    if result.face_landmarks:
-
-        face = result.face_landmarks[0]
-        landmark_data = []
-
-        for landmark in face:
-            landmark_data.extend([
-                landmark.x,
-                landmark.y,
-                landmark.z
-            ])
-
-        fitur = np.array(
-            landmark_data,
-            dtype=np.float32
+        if not berhasil:
+            print("Gagal membaca kamera.")
+            break
+        frame = cv2.flip(frame, 1)
+        frame_rgb = cv2.cvtColor(
+            frame,
+            cv2.COLOR_BGR2RGB
         )
-        jarak = []
+        mp_image = mp.Image(
+            image_format=mp.ImageFormat.SRGB,
+            data=frame_rgb
+        )
+        result = landmarker.detect(mp_image)
 
-        for centroid in centroids:
-            distance = np.linalg.norm(
-                fitur - centroid
+        arah = "TIDAK TERDETEKSI"
+
+        if result.face_landmarks:
+
+            face = result.face_landmarks[0]
+            landmark_data = []
+
+            for landmark in face:
+                landmark_data.extend([
+                    landmark.x,
+                    landmark.y,
+                    landmark.z
+                ])
+
+            fitur = np.array(
+                landmark_data,
+                dtype=np.float32
             )
-            jarak.append(distance)
+            jarak = []
 
-        index_terdekat = np.argmin(jarak)
+            for centroid in centroids:
+                distance = np.linalg.norm(
+                    fitur - centroid
+                )
+                jarak.append(distance)
 
-        arah = classes[index_terdekat].upper()
+            index_terdekat = np.argmin(jarak)
+
+            arah = classes[index_terdekat].upper()
 
 
-    cv2.putText(
-        frame,
-        f"Arah: {arah}",
-        (30, 50),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1.2,
-        (0, 255, 0),
-        3
-    )
+        cv2.putText(
+            frame,
+            f"Arah: {arah}",
+            (30, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1.2,
+            (0, 255, 0),
+            3
+        )
 
-    cv2.imshow(
-        "Deteksi Arah Wajah",
-        frame
-    )
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+        cv2.imshow(
+            "Deteksi Arah Wajah",
+            frame
+        )
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    except KeyboardInterrupt:
+        print("Program dihentikan.")
         break
 
 kamera.release()
